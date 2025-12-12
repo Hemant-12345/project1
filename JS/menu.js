b16e80b49688c35e2ff4fa99be0b226555dd785e
Hemant-12345 <hemantsharma2435@gmail.com>
2025-12-12 15:04:28 +0530

    Update menu.js

--- JS/menu.js
+++ JS/menu.js
@@ -1,1 +1,22 @@
 
+ fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById("header").innerHTML = data;
+
+       let currentPage = window.location.pathname.split("/").pop();
+            if ( !currentPage || currentPage == "#"){
+                currentPage = "index.html";
+            }
+       let links = document.querySelectorAll(".menu .menubar ul li a");
+      
+       links.forEach(link => {
+           console.log("current page:", currentPage);
+           let linkPage = link.getAttribute("href");
+           console.log("Attribute Value:", linkPage);
+           if(!linkPage) return;
+           if(linkPage == currentPage){
+               link.closest("li").classList.add("underline_current");
+           }
+       });
+    });
