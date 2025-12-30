72aa6c8d03ba1d361cd0ef2d9a73448a8fb6fdfd
Hemant-12345 <hemantsharma112004@gmail.com>
2025-12-30 13:03:58 +0530

    Update menu.js

--- JS/menu.js
+++ JS/menu.js
@@ -1,45 +1,42 @@
-fetch("header.html")
-.then(res => res.text())
-.then(data => {
-	document.getElementById("header").innerHTML = data;
-    let currentPage = window.location.pathname.split("/").pop();
-    if ( !currentPage || currentPage == "#" ) {
-		currentPage = "index.html";
-    }
-    // menu me anchor tak pahuchne ka tareeka
-    let links = document.querySelectorAll(".menu .menubar ul li a");
-        
-	// hr anchor tag ko process karenge
-    links.forEach(link => {
-		//pehle  anchor tag ka href ki value save karenge
-    	let linkPage = link.getAttribute("href");
-		
-        if(!linkPage) return;
-  
-        if (linkPage == currentPage) {
-      		link.closest("li").classList.add("underline_current");
-    	}
+fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById("header").innerHTML = data;
 
-		if (window.innerWidth <= 576) {
-			const menu = document.querySelector(".menu");
-			const menuBar = document.querySelector(".menu .menubar ul");
-			const mobileBar = document.querySelector(".menu .mobilebar");
+       let currentPage = window.location.pathname.split("/").pop();
+            if ( !currentPage || currentPage == "#"){
+                currentPage = "index.html";
+            }
+       let links = document.querySelectorAll(".menu .menubar ul li a");
+      
+       links.forEach(link => {
+           console.log("current page:", currentPage);
+           let linkPage = link.getAttribute("href");
+           console.log("Attribute Value:", linkPage);
+           if(!linkPage) return;
+           if(linkPage == currentPage){
+               link.closest("li").classList.add("underline_current");
+           }
+           if (window.innerWidth <= 576) {
+               const menu = document.querySelector(".menu");
+               const menuBar = document.querySelector(".menu .menubar");
+               const mobileBar = document.querySelector(".menu .mobilebar");
 
-			if(mobileBar && !mobileBar.querySelector(".hamburger")) {
-				const dv = document.createElement("div");
-				dv.className = "hamburger";
-				dv.setAttribute("aria-label", "Toggle menu");
-				dv.setAttribute("tabindex","0");
-				dv.innerHTML= '<span></span>';
-				mobileBar.appendChild(dv);
+                if (mobileBar && !mobileBar.querySelector(".hamburger")){
+                   const dv = document.createElement("div")
+                   dv.className = "hamburger";
+                   dv.setAttribute("aria-label", "Toggle menu");
+                   dv.setAttribute("tabindex", "0");
+                   dv.innerHTML = '<span></span>';
+                   mobileBar.appendChild(dv)
 
-				dv.addEventListener("click", function(e) {
-					e.stopPropagation();
-					menu.classList.toggle("mobile-open");
-				});
-			}
 
-			// sabse pehle --> menu items nikalne padenge
+                   dv.addEventListener("click", function(e){
+                      e.stopPropagation();
+                      menu.classList.toggle("mobile-open");
+                   });
+                }
+                // sabse pehle --> menu items nikalne padenge
 			const menuList = document.querySelectorAll(".menu .menubar ul li");
 			
 			menuList.forEach(li => {
@@ -65,8 +62,6 @@ fetch("header.html")
 					
 				}
 			});
-			
-		}
-	});
-
-}) 
+           }
+       });
+    });
