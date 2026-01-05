3256b2f752a2a7e26571ca47eef93a33b5f7770f
Hemant-12345 <hemantsharma112004@gmail.com>
2026-01-05 18:45:40 +0530

    Update menu.js

--- JS/menu.js
+++ JS/menu.js
@@ -56,13 +56,23 @@ fetch("navbar.html")
 					const firstLink = li.querySelector("a");
 					firstLink.after(toggle);
 
+					// fouth step --> close already open / close all submenu
+						li.addEventListener("click", function (ev) {
+							ev.stopPropagation();
+							menuList.forEach(s_li =>{
+								const s_submenuExists = s_li.querySelector(".submenu");
+								if(s_submenuExists && s_li !== li){
+									console.log(s_submenuExists);
+									console.log(s_li);
+									s_li.classList.remove("open-submenu");
+								}
+							});
 					// fifth step --> submenu display karna jo menu item click hua ho
 					li.addEventListener("click", function (ev) {
 						ev.stopPropagation();
 						const isOpen = li.classList.toggle("open-submenu");
 						toggle.setAttribute("aria-expanded", isOpen? "true" : "false");
 					});
-					// fouth step --> close already open / close all submenu
 					}
 				}
 			});
