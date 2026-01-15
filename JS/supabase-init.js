c8471d6d0834c008745a20268a66997cc70e84b8
Hemant-12345 <hemantsharma112004@gmail.com>
2026-01-15 10:10:56 +0530

    Create supabase-init.js

---
+++ JS/supabase-init.js
@@ -0,0 +1,11 @@
+const suoabaseClient = window.supabase.createClient(
+  "https://duscmumrlnzwwbrxbhrh.supabase.co",
+  "sb_publishable_x4jMioMZ4TaeLRLLTHcVCg_8VpBy94c"
+);
+
+if (supabaseClient) {
+  console.log("Database Connected:", supabaseClient);
+}
+else {
+  console.log("Database not connected");
+}
